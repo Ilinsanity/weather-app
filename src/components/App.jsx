562aa5aa12f499d588f5bdae4724220add72d43c
Issac Lin <issaclin2002@gmail.com>
2022-05-12 18:58:44 -0400

    clean up elements from create-react-app

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Topbar from "./topbar"
+
+function App(){
+    return(
+        <div>
+            <Topbar />
+            <h1 className="heading1">Hello World</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/topbar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function Topbar(){
+
+
+    return(
+        <div>
+            <p>kjaiofssklals dskfdksfakfk</p>
+        </div>
+    )
+}
+
+export default Topbar;
